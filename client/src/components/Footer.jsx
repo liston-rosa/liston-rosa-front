ed7ef69ddf498efcfc6a7bedc89bcdc207fa3c10
Cris3h <cris.treachi@gmail.com>
2023-10-28 14:57:47 -0300

    feature: NavBar && Footer && Home component w stylesheet

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import instagram from "../../images/instagram.png";
+import facebook from "../../images/facebook.png";
+import styles from "../styles/footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const info = [
+  "mail icon",
+  "dirección icon",
+  "tel icon",
+  "whatsapp icon",
+  "redes 1 icon",
+  "redes 2 icon",
+];
+
+function Footer() {
+  return (
+    <div className={styles.footerMainContainer}>
+      <div className={styles.usContainer}>
+        <Link href="/us" target="_blank" rel="noopener noreferrer">
+          us
+        </Link>
+      </div>
+      <div className={styles.mediaContainer}>
+        <div className={styles.igContainer}>
+          <a
+            href="http://instagram.com/liston_rosa"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image src={instagram} alt="" />
+          </a>
+        </div>
+
+        <div className={styles.fbContainer}>
+          <Link
+            href="http://facebook.com/listonrosa"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image src={facebook} alt="" width={'auto'} height={'auto'} />
+          </Link>
+        </div>
+      </div>
+
+      <div className={styles.contactContainer}>
+        {info.map((e, i) => (
+          <p key={i}>{e}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
