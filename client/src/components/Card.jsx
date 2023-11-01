67022b31c9edcf8852b43c5dd329bf07811c358f
Cris3h <cris.treachi@gmail.com>
2023-11-01 20:09:04 -0300

    feature: card styles first version

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import styles from "../styles/artwork.module.css";
 import Link from "next/link";
-
 import styles from '../styles/card.module.css'
-
+import Image from "next/image";
+import muralG from '../images/muralG.jpg'
 const Card = ({ id, image, name, place, description }) => {
     // console.log()
 
@@ -11,7 +10,17 @@ const Card = ({ id, image, name, place, description }) => {
     <div className={styles.mainContainer}>
       <Link href={`/murales/${id}`} className={styles.cardLink}>
         <div className={styles.card}>
-          <h2>Obra: {name}</h2>
+          <Image 
+          src={muralG}
+          alt='proyecto'
+          width={'auto'}
+          height={'auto'}
+          priority
+          />
+          <div className={styles.descripContainer}>
+          <h4>{name}</h4>
+
+         
           <div className={styles.state}>
             <p>
               <strong>Provincia:</strong> {place?.state}
@@ -22,8 +31,9 @@ const Card = ({ id, image, name, place, description }) => {
               <strong>Ciudad:</strong> {place?.city}
             </p>
           </div>
+          </div>
           <div className={styles.description}>
-            <p>{description}</p>
+            <p>{description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam neque consequuntur unde quidem sunt deserunt reprehenderit beatae mollitia laudantium minus blanditiis aliquam vero debitis, non, quibusdam totam eius, veniam cum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus enim quidem quibusdam iusto deserunt facere sequi nesciunt quod, quisquam expedita, dolor illo, soluta asperiores adipisci incidunt laboriosam quam consequuntur illum.</p>
           </div>
         </div>
       </Link>
