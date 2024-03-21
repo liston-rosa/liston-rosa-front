c43f05976f9929661151178b70d923bad75c716c
Cris3h <cris.treachi@gmail.com>
2024-03-21 20:23:14 -0300

    web designe beta version

---
+++ src/components/GoogleLogin.jsx
@@ -0,0 +1,38 @@
+// 'use client'
+// import React, { useEffect } from "react";
+
+// import { signIn, signOut, useSession } from "next-auth/react";
+
+
+// const GoogleSignInButton = () => {
+//     // const { data: session, status } = useSession();
+
+//     // console.log("this is session --> ", session, "this is status ---> ", status);
+
+//     if(session){
+//         return(
+//             <>
+//             <p> welcome {session.user.email}</p>
+//             <button onClick={() => signOut()}> Sign out</button>
+//             </>
+//         )
+//     } else {
+//         return (
+//             <>
+//             <p>you are not logged in</p>
+//             <button onClick={()=> signIn()}> sign in</button>
+//             </>
+//         )
+//     }
+
+// };
+
+// export default GoogleSignInButton;
+
+
+
+
+
+
+
+
